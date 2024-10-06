663b4288ea0a3e1c6cf912d535c3270223ad3b88
Kittikon <84323781+Whitek323@users.noreply.github.com>
2024-10-06 15:14:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -165,7 +165,8 @@ def pdfPost():
     return response
 
 def start_app():
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    # app.run(host="0.0.0.0", port=8080, debug=True)
+    app.run(host="0.0.0.0", debug=False)
 
 
 if __name__ == "__main__":
